3b51a4633d05377c49702cdb64071a89f5bbe0f6
RenanEvangelista <renan103326773911@gmail.com>
2021-08-25 18:21:08 -0300

    feat: add AppError

---
+++ src/shared/errors/AppError.ts
@@ -0,0 +1,12 @@
+class AppError extends Error {
+  constructor(
+    public readonly message: string,
+    public readonly statusCode = 400,
+  ) {
+    super(message);
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default AppError;
