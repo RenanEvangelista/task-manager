800adb27e6a48a6052d4013104ebe7c638ecf205
RenanEvangelista <renan103326773911@gmail.com>
2021-08-25 18:32:19 -0300

    test: should not be able to create user with same email

--- tests/modules/users/services/CreateUserService.spec.ts
+++ tests/modules/users/services/CreateUserService.spec.ts
@@ -1,5 +1,6 @@
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUserRepository';
 import CreateUserService from '@modules/users/services/CreateUserService';
+import AppError from '@shared/errors/AppError';
 
 let fakeUsersRepository: FakeUsersRepository;
 let createUserService: CreateUserService;
@@ -12,11 +13,27 @@ describe('CreateUserService', () => {
 
   it('should be able to create user', async () => {
     const user = await createUserService.execute({
-      name: 'Renan Evangelista',
-      email: 'renan@gmail.com',
-      password: '545454Rr*',
+      name: 'Test User',
+      email: 'test@test.com',
+      password: '12345Rr*',
     });
 
     expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create user with same email', async () => {
+    await createUserService.execute({
+      name: 'Test User',
+      email: 'test@test.com',
+      password: '12345Rr*',
+    });
+
+    await expect(
+      createUserService.execute({
+        name: 'Test User 2',
+        email: 'test@test.com',
+        password: '123456Rr*',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
