12f9e508007f4dd7edc770136b9f273206a12d38
RenanEvangelista <renan103326773911@gmail.com>
2021-08-25 18:04:52 -0300

    feat: add find method to usersRepository contract

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -2,6 +2,7 @@ import User from '../models/user';
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
 
 interface IUsersRepository {
+  findByEmail(email: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
   save(user: User): Promise<User>;
 }
