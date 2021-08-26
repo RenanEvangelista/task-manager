623dd8c238c6fe140342b56ad687790d286fb373
RenanEvangelista <renan103326773911@gmail.com>
2021-08-26 00:22:41 -0300

    feat: add create method to ITasksRepository

--- src/modules/tasks/repositories/ITasksRepository.ts
+++ src/modules/tasks/repositories/ITasksRepository.ts
@@ -1,7 +1,9 @@
 import User from '@modules/users/models/user';
+import ICreateTaskDTO from '../dtos/ICreateTaskDTO';
 import Task from '../models/task';
 
 export default interface ITasksRepository {
-  findById: (id: number) => Promise<Task | undefined>;
-  findByUserByDate: (user: User, date: Date) => Promise<Task[] | []>;
+  findById(id: number): Promise<Task | undefined>;
+  findByUserByDay(user: User, day: number): Promise<Task[] | []>;
+  create(data: ICreateTaskDTO): Promise<Task>;
 }
