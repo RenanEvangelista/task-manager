5d98fac2a5ca6258c651268069912dba780da64c
RenanEvangelista <renan103326773911@gmail.com>
2021-08-26 03:02:46 -0300

    feat: add user controller

---
+++ src/modules/users/controllers/usersController.ts
@@ -0,0 +1,22 @@
+import { container } from 'tsyringe';
+import { Response, Request } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+class usersController {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUserService = container.resolve(CreateUserService);
+
+    const user = await createUserService.execute({
+      email,
+      name,
+      password,
+    });
+
+    return response.json(user);
+  }
+}
+
+export default usersController;
