fa71ebc80fd5a5b7eb8ad0dd7ec358837b98c5ae
RenanEvangelista <renan103326773911@gmail.com>
2021-08-26 00:34:56 -0300

    feat: update method findById param to string

--- src/modules/tasks/repositories/ITasksRepository.ts
+++ src/modules/tasks/repositories/ITasksRepository.ts
@@ -3,7 +3,7 @@ import ICreateTaskDTO from '../dtos/ICreateTaskDTO';
 import Task from '../models/task';
 
 export default interface ITasksRepository {
-  findById(id: number): Promise<Task | undefined>;
-  findByUserByDay(user: User, day: number): Promise<Task[] | []>;
+  findById(id: string): Promise<Task | undefined>;
+  findByUserByDay(user: User, date: Date): Promise<Task[] | []>;
   create(data: ICreateTaskDTO): Promise<Task>;
 }
