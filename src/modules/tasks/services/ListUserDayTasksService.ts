d20db4b457aa374f452bf3096118d9115d5ff353
RenanEvangelista <renan103326773911@gmail.com>
2021-08-26 02:44:07 -0300

    refactor: remove unecessary usersRepository injection

--- src/modules/tasks/services/ListUserDayTasksService.ts
+++ src/modules/tasks/services/ListUserDayTasksService.ts
@@ -1,6 +1,6 @@
 import { startOfDay } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
-import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
 import ITasksRepository from '../repositories/ITasksRepository';
 
 import Task from '../models/task';
@@ -15,16 +15,12 @@ class ListUserDayTasksService {
   constructor(
     @inject('TasksRepository')
     private readonly tasksRepository: ITasksRepository,
-
-    @inject('UsersRepository')
-    private readonly usersRepository: IUsersRepository,
   ) {}
 
   async execute({ user_id, date }: IRequest): Promise<Task[] | []> {
     const startDate = startOfDay(date);
 
     const list = await this.tasksRepository.findByUserByDay(user_id, startDate);
-
     return list;
   }
 }
