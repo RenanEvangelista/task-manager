4a3e6538b92aec1f22244075dcd31f67d5dcf83f
RenanEvangelista <renan103326773911@gmail.com>
2021-08-26 02:26:56 -0300

    feat: rename ITaskRepository param user to user_id

--- src/modules/tasks/repositories/ITasksRepository.ts
+++ src/modules/tasks/repositories/ITasksRepository.ts
@@ -3,6 +3,6 @@ import Task from '../models/task';
 
 export default interface ITasksRepository {
   findById(id: string): Promise<Task | undefined>;
-  findByUserByDay(user: string, date: Date): Promise<Task[] | []>;
+  findByUserByDay(user_id: string, date: Date): Promise<Task[] | []>;
   create(data: ICreateTaskDTO): Promise<Task>;
 }
