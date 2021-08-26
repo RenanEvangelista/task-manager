a069c6945e8ed5f363105505247d2f38f3d831c2
RenanEvangelista <renan103326773911@gmail.com>
2021-08-26 19:57:24 -0300

    feat: add task typeorm entitie

---
+++ src/shared/infra/database/typeorm/entities/Task.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import User from './User';
+
+@Entity('tasks')
+class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('time with time zone')
+  date: Date;
+
+  @Column()
+  description: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Task;
