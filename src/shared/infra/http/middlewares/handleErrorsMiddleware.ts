11016d4c57a3665b5d6b979e804ec844d22794a1
RenanEvangelista <renan103326773911@gmail.com>
2021-08-26 18:23:54 -0300

    chore: add handle generic error

--- src/shared/infra/http/middlewares/handleErrorsMiddleware.ts
+++ src/shared/infra/http/middlewares/handleErrorsMiddleware.ts
@@ -2,7 +2,7 @@ import AppError from '@shared/errors/AppError';
 import { Request, Response, NextFunction } from 'express';
 
 const handleErrorsMiddleware = (
-  err: AppError,
+  err: Error,
   _request: Request,
   response: Response,
   next: NextFunction,
@@ -11,6 +11,14 @@ const handleErrorsMiddleware = (
     return response.status(err.statusCode).json({
       status: 'error',
       message: err.message,
+    });
+  }
+
+  if (err instanceof Error) {
+    console.log(err);
+    return response.status(500).json({
+      status: 'error',
+      message: 'internal server error',
     });
   }
 
