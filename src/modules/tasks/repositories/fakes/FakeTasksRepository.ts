9aef6ac99a9302134ab210e345780088462a346f
RenanEvangelista <renan103326773911@gmail.com>
2021-08-26 02:17:50 -0300

    feat: should not be able to create task with a non-existing user

---
+++ src/modules/tasks/repositories/fakes/FakeTasksRepository.ts
@@ -0,0 +1,46 @@
+import ICreateTaskDTO from '@modules/tasks/dtos/ICreateTaskDTO';
+import Task from '@modules/tasks/models/task';
+import { startOfDay, differenceInDays } from 'date-fns';
+import { uuid } from 'uuidv4';
+import ITasksRepository from '../ITasksRepository';
+
+class FakeTasksRepository implements ITasksRepository {
+  private tasks: Task[] = [];
+
+  async findById(id: string): Promise<Task | undefined> {
+    const task = this.tasks.find((taskFind) => taskFind.id === id);
+
+    return task;
+  }
+
+  async findByUserByDay(user_id: string, date: Date): Promise<Task[] | []> {
+    const tasks = this.tasks.filter(
+      (task) => user_id === task.user_id && !differenceInDays(date, task.date),
+    );
+
+    return tasks;
+  }
+
+  async create({
+    name,
+    date,
+    user_id,
+    description,
+  }: ICreateTaskDTO): Promise<Task> {
+    const task: Task = {
+      id: uuid(),
+      name,
+      user_id,
+      date: startOfDay(date),
+      description,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    this.tasks.push(task);
+
+    return task;
+  }
+}
+
+export default FakeTasksRepository;
