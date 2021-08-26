24ac6bff5d31bbde7dacc4515ff7d81bbc6836e1
RenanEvangelista <renan103326773911@gmail.com>
2021-08-25 23:32:51 -0300

    feat: add AuthenticaUserService

---
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -0,0 +1,45 @@
+import { inject, injectable } from 'tsyringe';
+import { sign } from 'jsonwebtoken';
+
+import authConfig from '@config/auth';
+import User from '../models/user';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  token: string;
+  user: User;
+}
+@injectable()
+class AuthenticateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private readonly usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<Response> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('gafgh');
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
