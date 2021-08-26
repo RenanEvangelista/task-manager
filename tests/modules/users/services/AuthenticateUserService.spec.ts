14b9c9f3662a02e97c3664fa16769e81a5efed96
RenanEvangelista <renan103326773911@gmail.com>
2021-08-25 23:52:38 -0300

    test: should not be able to authenticate user with incorrect password

--- tests/modules/users/services/AuthenticateUserService.spec.ts
+++ tests/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,21 +1,27 @@
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUserRepository';
+import FakeHashProvider from '@shared/providers/HashProvider/FakeHashProvider';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 import AppError from '@shared/errors/AppError';
 
 let authenticateUser: AuthenticateUserService;
 let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
 
 describe('AuthenticateUserService', () => {
   beforeEach(() => {
+    fakeHashProvider = new FakeHashProvider();
     fakeUsersRepository = new FakeUsersRepository();
-    authenticateUser = new AuthenticateUserService(fakeUsersRepository);
+    authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
   });
 
   it('should be able to authenticate user', async () => {
     const user = await fakeUsersRepository.create({
       name: 'Test User',
       email: 'test@test.com',
-      password: '12345Rr*',
+      password: '12345Rr*12345',
     });
 
     const response = await authenticateUser.execute({
@@ -32,6 +38,21 @@ describe('AuthenticateUserService', () => {
       authenticateUser.execute({
         email: 'test@test.com',
         password: '12345Rr*',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate user with incorrect password', async () => {
+    await fakeUsersRepository.create({
+      email: 'test@test.com',
+      name: 'Test User',
+      password: '12345Rr*',
+    });
+
+    await expect(
+      authenticateUser.execute({
+        email: 'test@test.com',
+        password: 'incorrectPassword',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
