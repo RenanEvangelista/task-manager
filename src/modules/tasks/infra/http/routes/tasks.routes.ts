7aaf934234cf640ca00484fcd6b5215f195ecb6d
RenanEvangelista <renan103326773911@gmail.com>
2021-08-26 04:20:16 -0300

    feat: add tasks routes

---
+++ src/modules/tasks/infra/http/routes/tasks.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
+import TasksController from '../controllers/TasksController';
+
+const tasksRouter = Router();
+const tasksController = new TasksController();
+
+tasksRouter.use(ensureAuthenticated);
+
+tasksRouter.post('/', tasksController.create);
+
+export default tasksRouter;
