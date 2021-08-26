d1674909fb06dfc1a4ffba45243459df771cf747
RenanEvangelista <renan103326773911@gmail.com>
2021-08-26 00:40:27 -0300

    feat: update ITasksRepository param user to user_id

--- src/modules/tasks/repositories/ITasksRepository.ts
+++ src/modules/tasks/repositories/ITasksRepository.ts
@@ -1,9 +1,8 @@
-import User from '@modules/users/models/user';
 import ICreateTaskDTO from '../dtos/ICreateTaskDTO';
 import Task from '../models/task';
 
 export default interface ITasksRepository {
   findById(id: string): Promise<Task | undefined>;
-  findByUserByDay(user: User, date: Date): Promise<Task[] | []>;
+  findByUserByDay(user: user_id, date: Date): Promise<Task[] | []>;
   create(data: ICreateTaskDTO): Promise<Task>;
 }
