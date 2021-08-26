e7e7cfc813e747f3c6ba88b8da5c8040e6157950
RenanEvangelista <renan103326773911@gmail.com>
2021-08-26 17:36:48 -0300

    feat: import sessions routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import tasksRouter from '@modules/tasks/infra/http/routes/tasks.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/tasks', tasksRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
