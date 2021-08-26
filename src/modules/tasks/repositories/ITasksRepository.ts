e79c3e050986c8728f8144fd430612c88b906f96
RenanEvangelista <renan103326773911@gmail.com>
2021-08-26 00:41:35 -0300

    refactor: fix type to user param on ItasksRepository

--- src/modules/tasks/repositories/ITasksRepository.ts
+++ src/modules/tasks/repositories/ITasksRepository.ts
@@ -3,6 +3,6 @@ import Task from '../models/task';
 
 export default interface ITasksRepository {
   findById(id: string): Promise<Task | undefined>;
-  findByUserByDay(user: user_id, date: Date): Promise<Task[] | []>;
+  findByUserByDay(user: string, date: Date): Promise<Task[] | []>;
   create(data: ICreateTaskDTO): Promise<Task>;
 }
