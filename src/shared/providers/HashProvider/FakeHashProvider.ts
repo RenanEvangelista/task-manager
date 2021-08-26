4f71bdb0b7d8d71acd8a2c0ca0af98c4d04740f5
RenanEvangelista <renan103326773911@gmail.com>
2021-08-25 23:13:53 -0300

    refactor: update generate method from fakeHashProvider

--- src/shared/providers/HashProvider/FakeHashProvider.ts
+++ src/shared/providers/HashProvider/FakeHashProvider.ts
@@ -1,8 +1,10 @@
+import crypto from 'crypto';
+
 import IHashProvider from './IHashProvider';
 
 class FakeHashProvider implements IHashProvider {
   async generate(payload: string): Promise<string> {
-    return payload;
+    return crypto.createHash('md5').update(payload).digest('hex');
   }
 
   async compare(payload: string, hash: string): Promise<boolean> {
