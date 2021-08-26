dffb7e8363c597a343e4cd7cd900a922a34f2afe
RenanEvangelista <renan103326773911@gmail.com>
2021-08-25 22:41:30 -0300

    feat: add fakeHashProvider

---
+++ src/shared/providers/HashProvider/FakeHashProvider.ts
@@ -0,0 +1,13 @@
+import IHashProvider from './IHashProvider';
+
+class FakeHashProvider implements IHashProvider {
+  async generate(payload: string): Promise<string> {
+    return payload;
+  }
+
+  async compare(payload: string, hash: string): Promise<boolean> {
+    return payload === hash;
+  }
+}
+
+export default FakeHashProvider;
