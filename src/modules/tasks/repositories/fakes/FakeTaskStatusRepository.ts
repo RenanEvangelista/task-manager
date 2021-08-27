7d5ccbb9f96bf2c35a449e42a41cebae727da3f6
RenanEvangelista <renan103326773911@gmail.com>
2021-08-27 05:16:37 -0300

    feat: add fake task status repository

--- src/modules/tasks/repositories/fakes/FakeTaskStatusRepository.ts
+++ src/modules/tasks/repositories/fakes/FakeTaskStatusRepository.ts
@@ -1,14 +1,32 @@
 import TaskStatus from '@modules/tasks/models/TaskStatus';
+import { v4 } from 'uuid';
 import ITaskStatusRepository from '../ITaskStatusRepository';
 
 class FakeTaskStatusRepository implements ITaskStatusRepository {
   private statusStorage: TaskStatus[] = [];
 
-  async findById(id: string): Promise<TaskStatus | undefined> {
-    throw new Error('Method not implemented.');
+  async findAll(): Promise<TaskStatus[]> {
+    return this.statusStorage;
   }
 
-  async create(name: string): Promise<TaskStatus> {
-    throw new Error('Method not implemented.');
+  async findById(id: string): Promise<TaskStatus | undefined> {
+    const status = this.statusStorage.find((st) => st.id === id);
+
+    return status;
+  }
+
+  async create(title: string): Promise<TaskStatus> {
+    const status = {
+      id: v4(),
+      title,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    this.statusStorage.push(status);
+
+    return status;
   }
 }
+
+export default FakeTaskStatusRepository;
