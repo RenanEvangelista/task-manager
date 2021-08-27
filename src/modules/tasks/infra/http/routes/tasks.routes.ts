6bf0972f662bbef6eb4e5422d493448e5f065d45
RenanEvangelista <renan103326773911@gmail.com>
2021-08-27 01:21:23 -0300

    feat: add userTasksMonth routes

--- src/modules/tasks/infra/http/routes/tasks.routes.ts
+++ src/modules/tasks/infra/http/routes/tasks.routes.ts
@@ -4,14 +4,17 @@ import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAut
 
 import TasksController from '../controllers/TasksController';
 import UserTasksDayController from '../controllers/UserTasksDayController';
+import UserTasksMonthController from '../controllers/UserTasksMonthController';
 
 const tasksRouter = Router();
 const tasksController = new TasksController();
 const userTasksDayController = new UserTasksDayController();
+const userTasksMonthController = new UserTasksMonthController();
 
 tasksRouter.use(ensureAuthenticated);
 
 tasksRouter.post('/', tasksController.create);
-tasksRouter.get('/', userTasksDayController.index);
+tasksRouter.get('/day', userTasksDayController.index);
+tasksRouter.get('/month', userTasksMonthController.index);
 
 export default tasksRouter;
