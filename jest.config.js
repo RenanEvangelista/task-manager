5cc8ab1ac7cc7636ae51d67ce9ae4e193f61ddc1
RenanEvangelista <renan103326773911@gmail.com>
2021-08-26 23:44:04 -0300

    chore: change jest collect coverage from

--- jest.config.js
+++ jest.config.js
@@ -23,7 +23,7 @@ module.exports = {
    collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-   collectCoverageFrom: ['<rootDir>/tests'],
+   collectCoverageFrom: ['<rootDir>/src/modules/**/services/*.ts'],
 
   // The directory where Jest should output its coverage files
    coverageDirectory: 'covarage',
