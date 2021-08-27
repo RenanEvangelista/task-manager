e49ed50034ece41cbe8537f94594c18aefb2971c
RenanEvangelista <renan103326773911@gmail.com>
2021-08-27 03:45:33 -0300

    feat: add user tasks list by date interval routes

--- src/modules/tasks/infra/http/routes/tasks.routes.ts
+++ src/modules/tasks/infra/http/routes/tasks.routes.ts
@@ -5,15 +5,18 @@ import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAut
 import TasksController from '../controllers/TasksController';
 import UserTasksDayController from '../controllers/UserTasksDayController';
 import UserTasksMonthController from '../controllers/UserTasksMonthController';
+import UserTasksDateIntervalController from '../controllers/UserTasksDateIntervalController';
 
 const tasksRouter = Router();
 const tasksController = new TasksController();
 const userTasksDayController = new UserTasksDayController();
 const userTasksMonthController = new UserTasksMonthController();
+const userTasksDateIntervalController = new UserTasksDateIntervalController();
 
 tasksRouter.use(ensureAuthenticated);
 
 tasksRouter.post('/', tasksController.create);
+tasksRouter.get('/', userTasksDateIntervalController.index);
 tasksRouter.get('/day', userTasksDayController.index);
 tasksRouter.get('/month', userTasksMonthController.index);
 
