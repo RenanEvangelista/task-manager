e5c219a174a57a837a6a331469e2f5dae7be5e86
RenanEvangelista <renan103326773911@gmail.com>
2021-08-28 00:07:04 -0300

    feat: add departments create table migration

---
+++ src/shared/infra/database/typeorm/migrations/1630119507525-CreateDepartments.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateDepartments1630119507525
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'departments',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'owner_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'DepartmentsUser',
+            columnNames: ['owner_id'],
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('departments', 'DepartmentsUser');
+  }
+}
