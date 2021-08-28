94b03534ec79c02c96cfc5d6b81fe2d642335bd2
RenanEvangelista <renan103326773911@gmail.com>
2021-08-28 01:31:02 -0300

    feat: add users attribute to department model

--- src/modules/departments/model/Department.ts
+++ src/modules/departments/model/Department.ts
@@ -1,7 +1,10 @@
+import User from '@modules/users/models/user';
+
 export default interface Department {
   id: string;
   owner_id: string;
   name: string;
+  users: User[];
   created_at: Date;
   updated_at: Date;
 }
