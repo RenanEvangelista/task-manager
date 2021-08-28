af172e5b801a21a5733621aa0e4bd0c1196797b6
RenanEvangelista <renan103326773911@gmail.com>
2021-08-28 01:32:14 -0300

    feat: add addUser method to department repository contract

--- src/modules/departments/repositories/IDepartmentsRepository.ts
+++ src/modules/departments/repositories/IDepartmentsRepository.ts
@@ -1,3 +1,4 @@
+import User from '@modules/users/models/user';
 import Department from '../model/Department';
 
 import ICreateDepartmentDTO from '../dtos/ICreateDepartmentDTO';
@@ -5,6 +6,7 @@ import ICreateDepartmentDTO from '../dtos/ICreateDepartmentDTO';
 interface IDepartmentsRepository {
   findById(id: string): Promise<Department | undefined>;
   create(data: ICreateDepartmentDTO): Promise<Department>;
+  addUser(department: Department, user: User): Promise<Department>;
 }
 
 export default IDepartmentsRepository;
