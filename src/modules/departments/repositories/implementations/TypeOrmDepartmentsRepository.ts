fdc50eabb695efef1b7f577e5f0d4258baeffde9
RenanEvangelista <renan103326773911@gmail.com>
2021-08-28 01:33:42 -0300

    feat: add addUser method to deparment typeorm repository

--- src/modules/departments/repositories/implementations/TypeOrmDepartmentsRepository.ts
+++ src/modules/departments/repositories/implementations/TypeOrmDepartmentsRepository.ts
@@ -2,6 +2,7 @@ import ICreateDepartmentDTO from '@modules/departments/dtos/ICreateDepartmentDTO
 import Department from '@modules/departments/model/Department';
 import { getRepository, Repository } from 'typeorm';
 import DepartmentEntity from '@shared/infra/database/typeorm/entities/Department';
+import User from '@shared/infra/database/typeorm/entities/User';
 import IDepartmentsRepository from '../IDepartmentsRepository';
 
 class TypeOrmDepartmentsRepository implements IDepartmentsRepository {
@@ -12,13 +13,28 @@ class TypeOrmDepartmentsRepository implements IDepartmentsRepository {
   }
 
   async findById(id: string): Promise<Department | undefined> {
-    const department = await this.departmentsRepository.findOne(id);
+    const department = await this.departmentsRepository.findOne(id, {
+      relations: ['users'],
+    });
 
     return department;
   }
 
   async create(data: ICreateDepartmentDTO): Promise<Department> {
     const department = this.departmentsRepository.create(data);
+
+    await this.departmentsRepository.save(department);
+
+    return department;
+  }
+
+  async addUser(
+    departmentData: DepartmentEntity,
+    user: User,
+  ): Promise<Department> {
+    const department = departmentData;
+
+    department.users = [...(departmentData.users ?? []), user];
 
     await this.departmentsRepository.save(department);
 
