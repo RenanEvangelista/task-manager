4945dca6b89716a2566cf6d992e787efc578b34f
RenanEvangelista <renan103326773911@gmail.com>
2021-08-28 01:34:44 -0300

    feat: add adduser department to department routes

--- src/modules/departments/infra/http/routes/departments.routes.ts
+++ src/modules/departments/infra/http/routes/departments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import DepartmentsController from '../controllers/DepartmentsController';
+import DepartmentUsersController from '../controllers/DepartmentUsersController';
 
 const departmentsRouter = Router();
 const departmentsController = new DepartmentsController();
+const departmentUsersController = new DepartmentUsersController();
 
 departmentsRouter.use(ensureAuthenticated);
 
 departmentsRouter.post('/', departmentsController.create);
+departmentsRouter.post('/users', departmentUsersController.update);
 
 export default departmentsRouter;
